d8b4b2effd9ea059c0eb5ef99bf1b34228e7c88d
Mark Hurenkamp <mark.hurenkamp@xs4all.nl>
2022-03-25 13:55:00 +0100

    Update config file locations, and update users & groups on boot

---
+++ scripts/del-groups.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create groups
+while read LINE
+do
+  value=(`echo $LINE | sed 's/:/\n/g'`)
+  group=${value[0]}
+  grep "^$group:" /etc/group
+  if [ $? -ne 1 ]; then
+    echo "deleting group $group" 
+    #delgroup $group
+  else
+    echo "group $group not found, skipping"
+  fi 
+done
+
